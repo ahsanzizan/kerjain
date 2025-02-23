6663413f2d54f7118efeefd43d962888d0d39074
Ahsan <ahsanaz461@gmail.com>
2025-02-23 17:06:53 +0700

    fix: Reduce horizontal padding in auth layout

--- src/components/features/auth/layout/auth.layout.tsx
+++ src/components/features/auth/layout/auth.layout.tsx
@@ -6,13 +6,13 @@ import { Carousel } from "../components/carousel";
 export const AuthLayout: FC<{ children?: ReactNode }> = ({ children }) => {
   return (
     <PageContainer>
-      <div className="grid min-h-screen w-full grid-cols-1 lg:grid-cols-2 [&>section]:px-14 [&>section]:py-20">
+      <div className="grid min-h-screen w-full grid-cols-1 lg:grid-cols-2 [&>section]:px-5 [&>section]:py-20 lg:[&>section]:px-14">
         <section id="login-form" className="w-full">
           {children}
         </section>
         <section
           id="guides"
-          className="bg-primary-500 flex max-w-full flex-col"
+          className="flex max-w-full flex-col bg-primary-500"
         >
           <Image
             src={"/logo-white.png"}
