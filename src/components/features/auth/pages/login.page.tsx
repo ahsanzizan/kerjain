37486cdcd8d3e9ad87ef81fe33ed7924f697a563
Ahsan <ahsanaz461@gmail.com>
2025-02-23 16:36:52 +0700

    fix: Wrap login page with Suspense to prevent nextjs yells about it

--- src/components/features/auth/pages/login.page.tsx
+++ src/components/features/auth/pages/login.page.tsx
@@ -1,9 +1,10 @@
 import { Text } from "@/components/common/text";
+import { Suspense } from "react";
 import { LoginForm } from "../components/forms/login.form";
 
 export const LoginPage = () => {
   return (
-    <>
+    <Suspense>
       <div className="mb-[3.75rem]">
         <Text variant="title1-bold" className="mb-1 text-primary-500">
           Selamat Datang di Kerjain!
@@ -14,6 +15,6 @@ export const LoginPage = () => {
         </Text>
       </div>
       <LoginForm />
-    </>
+    </Suspense>
   );
 };
